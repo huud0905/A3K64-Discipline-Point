f2756ce9bfce79664d9e430aea25740e0ac61ecb
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-05-18 20:49:31 +0700

    Enable task view enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './taskViewEnhancer';
 import './index.css';
 import './scoreboard-modal-fix.css';
 import './scoreboard-final-fix.css';
 import './scoreboard-theme-animation.css';
 import './scoreboard-final-polish.css';
+import './task-view.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
