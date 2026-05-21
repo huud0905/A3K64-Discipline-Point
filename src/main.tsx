22467ed332e9cda9960a7059b32f7aadfde1564d
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-05-21 15:35:37 +0700

    Remove legacy Profile shell import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './gasFetchCompat';
 import App from './App';
 import './personalizationSync';
-import './profileNativeShell';
 import './taskViewEnhancer';
 import './startMenuListEnhancer';
 import './mobileAppShell';
