1dd30bb6b2ebf13f6ba19799dff1287a6f7a0b9e
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-05-24 22:51:59 +0700

    Load separated mobile Settings runtime

--- src/scoreboardModalInputCleanup.ts
+++ src/scoreboardModalInputCleanup.ts
@@ -1,6 +1,7 @@
 import './settingsCustomColorPicker.css';
 import './settingsCustomColorPicker';
 import './accentPersistenceFix';
+import './settingsMobileSplitRuntime';
 
 function getScoreModal() {
   return document.querySelector<HTMLElement>('.score-edit-modal.modern-score-editor');
