88a5c5f8afb968ddc81a582d699085d0ccf3942e
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-05-24 21:17:26 +0700

    Load mobile polish CSS for Settings app

--- src/main.tsx
+++ src/main.tsx
@@ -50,6 +50,7 @@ import './scoreboardMobileScoreEditorFix.css';
 import './scoreboardMobileLightThemeHardFix.css';
 import './scoreboardMobileScoreEditorRuntimeFix';
 import './scoreboardPositiveColor.css';
+import './settingsMobileAppPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
