34dc0113675a6f47669595243617d8be73061cc4
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-06-14 00:10:26 +0700

    Load seating runtime hard fix

--- src/main.tsx
+++ src/main.tsx
@@ -86,6 +86,7 @@ import './seatingCanonicalNamesPatch';
 import './seatingPublishLitePatch';
 import './seatingViewerGateLitePatch';
 import './seatingAccessOverlayFix';
+import './seatingRuntimeHardFix';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
