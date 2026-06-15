161db2feadb4da8d974c19de78f54756f453a5c5
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-06-15 14:47:34 +0700

    refactor: group desktop runtime imports

---
+++ src/core/runtime/desktop.ts
@@ -0,0 +1,7 @@
+import '../../personalizationSync';
+import '../../appDataPreloader';
+import '../../desktopInteractionFix';
+import '../../profileWeekTableFix';
+import '../../taskViewEnhancer';
+import '../../startMenuListEnhancer';
+import '../../mobileAppShell';
