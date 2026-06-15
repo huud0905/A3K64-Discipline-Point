1ec62f34a07d26238c617affa99a9ac5a0848a22
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2026-06-15 13:01:57 +0700

    refactor: add core bootstrap imports

---
+++ src/core/bootstrap.ts
@@ -0,0 +1,63 @@
+import '../browserGlobalGuards';
+import '../gasFetchCompat';
+import '../gasSaveJsonpChunkPatch';
+
+import '../personalizationSync';
+import '../appDataPreloader';
+import '../desktopInteractionFix';
+import '../profileWeekTableFix';
+import '../taskViewEnhancer';
+import '../startMenuListEnhancer';
+import '../mobileAppShell';
+
+import '../scoreboardMobilePolish';
+import '../scoreboardMobileFinalFix';
+import '../scoreboardMobileOcdFix';
+import '../scoreboardMobileEmergencyPatch';
+import '../scoreboardMobileStudentDetailCardsPatch';
+import '../scoreboardModalInputCleanup';
+import '../scoreboardExcelExport';
+import '../scoreboardScreenshotCapture';
+import '../scoreboardScreenshotAllClassCompact';
+import '../scoreboardScreenshotGroupFullDetailsFix';
+import '../scoreboardMobileScoreEditorRuntimeFix';
+
+import '../overlayThemeSync';
+import '../globalLoadingThemeFix';
+
+import '../settingsDisplayOptionsPatch';
+
+import '../messagesThreadActionsPatch';
+import '../messagesSuggestionOpenPatch';
+import '../messagesPermissionRoleGuard';
+import '../messagesBubbleActionsPatch';
+import '../messagesServerReloadHardFix';
+
+import '../desktopDriveWallpaperSync';
+import '../desktopDriveErrorPolish';
+import '../desktopWallpaperStabilityFix';
+
+import '../profileWeekScoreChartPatch';
+
+import '../seatingMapStableApp';
+import '../seatingRoleGate';
+import '../seatingInteractionToolsPatch';
+import '../seatingSeatSwapHardPatch';
+import '../seatingRestoreAndControlsPatch';
+import '../seatingBackendActiveSync';
+import '../seatingOpenFeedbackPatch';
+import '../seatingNameHighlightPatch';
+import '../seatingCanonicalNamesPatch';
+import '../seatingPublishLitePatch';
+import '../seatingToolbarStable';
+import '../seatingEditNoRerenderPatch';
+import '../seatingAutoSave';
+import '../seatingNotificationDock';
+import '../seatingUiOcdFinalFix';
+import '../seatingFinalInteractionFix';
+import '../seatingPrintExportA4';
+import '../seatingViewerGateLitePatch';
+import '../seatingPrivacyWording';
+import '../seatingPublishCleanUiFix';
+import '../seatingAccessOverlay';
+import '../seatingRuntimeControls';
